853f68bdafb5b9872c1a4b20c15953952a374bdc
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2025-09-12 17:14:17 +0300

    refactor: always allow dueling when developing

--- server/utils/datetime.ts
+++ server/utils/datetime.ts
@@ -3,6 +3,9 @@
  * @returns True if it is the day before the weekly random duels, false otherwise.
  */
 export function isItTheDayBeforeWeeklyRandomDuels() {
+  // In development mode, always return `false`.
+  if (import.meta.dev) return false;
+
   const now = dayjs().utc();
   const [day, hour] = [now.day(), now.hour()];
   return (day === 5 && hour >= 7) || (day === 6 && hour <= 7);
