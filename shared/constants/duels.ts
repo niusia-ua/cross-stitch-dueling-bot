7ea221cb41406d1eef4d1f2422c71d6f5901a914
Nazar Antoniuk <nazarantoniuk18@gmail.com>
2025-10-01 11:50:11 +0300

    fix: set correct duel period

--- shared/constants/duels.ts
+++ shared/constants/duels.ts
@@ -4,7 +4,7 @@ export const HOUR = MINUTE * 60;
 export const DAY = HOUR * 24;
 
 export const DUEL_REQUEST_VALIDITY_PERIOD = HOUR;
-export const DUEL_PERIOD = SECOND * 10;
+export const DUEL_PERIOD = DAY;
 
 export const DUEL_REPORT_REMINDER_TIMEOUTS = [
   HOUR * 20, // 4 hours before the deadline.
